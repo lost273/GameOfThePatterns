8ea9bb81b95728edcde496193d6b49784cd74e4e
lost273 <dnk273@gmail.com>
2018-04-09 19:42:42 +0300

    Initial commit

---
+++ GameOfThePatterns/EventClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfThePatterns {
+    class EventClass {
+        public delegate void MethodContainer();
+        public event MethodContainer onCount;
+        public void EventGame() {
+            for (int i = 0; i < 100; i++) {
+                Console.Write("#");
+            }
+            onCount();
+        }
+    }
+    class Handler {
+        public void SomeMessage() {
+            Console.WriteLine("Something!");
+        }
+    }
+    class LetsEventGame {
+        public static void Start() {
+            EventClass letsPlay = new EventClass();
+            Handler handlerOne = new Handler();
+            letsPlay.onCount += handlerOne.SomeMessage;
+            letsPlay.EventGame();
+        }
+    }
+}
---
+++ GameOfThePatterns/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GameOfThePatterns {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello! Choose the game.");
+            Console.WriteLine("1. Event pattern.");
+            switch (Console.ReadLine()) {
+                case "1":
+                    LetsEventGame.Start();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
