bc2341e6633664cb6669aa96ad3cb812ddfe5c41
lost273 <dnk273@gmail.com>
2018-04-14 20:27:29 +0300

    14-04

--- GameOfThePatterns/DecoratorClass.cs
+++ GameOfThePatterns/DecoratorClass.cs
@@ -26,17 +26,32 @@ namespace GameOfThePatterns {
         public PrinterDecorator(string n, Printer p) : base(n) {
             printer = p;
         }
-        
     }
     //additional functionality
     class ColorPrint : PrinterDecorator {
         public ColorPrint(Printer p) : base(p.Name + ", with color printing",p){
         }
-
     }
+    class MonoPrint : PrinterDecorator {
+        public MonoPrint(Printer p) : base(p.Name + ", with mono printing", p) {
+        }
+    }
+    class BorderlessPrint : PrinterDecorator {
+        public BorderlessPrint(Printer p) : base(p.Name + ", with borderless printing", p) {
+        }
+    }
+
     class LetsDecoratorGame {
         public static void Start() {
+            Printer printerHP = new LaserPrinter();
+            printerHP = new MonoPrint(printerHP);
 
+            Printer printerSamsung = new InkPrinter();
+            printerSamsung = new ColorPrint(printerSamsung);
+            printerSamsung = new MonoPrint(printerSamsung);
+            printerSamsung = new BorderlessPrint(printerSamsung);
+            Console.WriteLine($"First printer - {printerHP.Name}");
+            Console.WriteLine($"Second printer - {printerSamsung.Name}");
         }
     }
 }
--- GameOfThePatterns/Program.cs
+++ GameOfThePatterns/Program.cs
@@ -5,6 +5,7 @@ namespace GameOfThePatterns {
         static void Main(string[] args) {
             Console.WriteLine("Hello! Choose the game.");
             Console.WriteLine("1. Event pattern.");
+            Console.WriteLine("2. Decorator pattern.");
             switch (Console.ReadLine()) {
                 case "1":
                     LetsEventGame.Start();
